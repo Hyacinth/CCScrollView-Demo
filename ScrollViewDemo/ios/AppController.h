c128fc10b328c9dddd547ae4cda4b2328bea93fd
oneRain <onerain88@gmail.com>
2012-07-17 09:51:34 +0800

    Initial Commit

---
+++ ScrollViewDemo/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  ScrollViewDemoAppController.h
+//  ScrollViewDemo
+//
+//  Created by Tom on 12-7-17.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@end
+
